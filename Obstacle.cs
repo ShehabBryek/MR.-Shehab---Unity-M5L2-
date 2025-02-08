fe023197d60dd49b6716e55b78366add0a6d1b36
ShehabBryek <shehabbryek71@gmail.com>
2025-02-08 15:49:44 +0200

    Obstacle.cs

---
+++ Obstacle.cs
@@ -0,0 +1,33 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+ 
+public class Obstacle : MonoBehaviour
+{
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if(collision.tag == "Player")
+        {
+            Destroy(collision.gameObject);
+        }
+    }
+}
+
+Item.cs
+
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+ 
+public class Item : MonoBehaviour
+{
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.tag == "Player")
+        {
+            Player player = collision.GetComponent<Player>();
+            player.points++;
+            Destroy(this.gameObject);
+        }
+    }
+}
